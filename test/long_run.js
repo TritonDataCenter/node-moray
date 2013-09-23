fc70ccc7677929bfb08afea7fb74e51ce3fadc37
Mark Cavage <mcavage@gmail.com>
2013-09-23 15:59:33 +0000

    [minor] reformat to 4 spaces and pick up libuuid

--- test/long_run.js
+++ test/long_run.js
@@ -2,7 +2,7 @@
 
 var assert = require('assert-plus');
 var bunyan = require('bunyan');
-var uuid = require('node-uuid');
+var libuuid = require('libuuid');
 
 var moray = require('../lib');
 
@@ -10,77 +10,77 @@ var moray = require('../lib');
 
 ///--- Globals
 
-var BUCKET = 't' + uuid.v4().substr(0, 7);
+var BUCKET = 't' + libuuid.create().substr(0, 7);
 var CONFIG = {
-        index: {
-                foo: {
-                        type: 'string'
-                }
+    index: {
+        foo: {
+            type: 'string'
         }
+    }
 };
 
 
 
 ///--- Mainline
 (function main() {
-        var client = moray.createClient({
-                connectTimeout: 1000,
-                dns: {
-                        checkInterval: 2000,
-                        // resolvers: ['10.99.99.201'],
-                        timeout: 500
-                },
-                log: bunyan.createLogger({
-                        name: 'moray_client',
-                        level: process.env.LOG_LEVEL || 'info',
-                        stream: process.stdout
-                }),
-                maxConnections: 100,
-                retry: {retries: 2},
-                url: process.env.MORAY_URL || 'tcp://127.0.0.1:2020'
-        });
+    var client = moray.createClient({
+        connectTimeout: 1000,
+        dns: {
+            checkInterval: 2000,
+            // resolvers: ['10.99.99.201'],
+            timeout: 500
+        },
+        log: bunyan.createLogger({
+            name: 'moray_client',
+            level: process.env.LOG_LEVEL || 'info',
+            stream: process.stdout
+        }),
+        maxConnections: 100,
+        retry: {retries: 2},
+        url: process.env.MORAY_URL || 'tcp://127.0.0.1:2020'
+    });
 
-        client.once('connect', function () {
-                client.putBucket(BUCKET, CONFIG, function (init_err) {
-                        assert.ifError(init_err);
+    client.once('connect', function () {
+        client.putBucket(BUCKET, CONFIG, function (init_err) {
+            assert.ifError(init_err);
 
-                        var MAX = 25000;
-                        function run() {
-                                var done = 0;
-                                function cb(err) {
-                                        if (err) {
-                                                console.error('put failed: %s',
-                                                              err.toString());
-                                        }
+            var MAX = 25000;
+            function run() {
+                var done = 0;
+                function cb(err) {
+                    if (err) {
+                        console.error('put failed: %s',
+                                      err.toString());
+                    }
 
-                                        if (++done === MAX) {
-                                                function d_cb(derr) {
-                                                        assert.ifError(derr);
-                                                        client.close();
-                                                }
-                                                client.deleteBucket(BUCKET,
-                                                                    d_cb);
-                                        }
-                                }
-
-                                function put(k, v) {
-                                        function p_cb(err) {
-                                                assert.ifError(err);
-                                                client.getObject(BUCKET, k, cb);
-                                        }
-                                        client.putObject(BUCKET, k, v, p_cb);
-                                }
-
-                                for (var i = 0; i < MAX; i++) {
-                                        var _k = uuid.v1();
-                                        var _v = {
-                                                foo: '' + i
-                                        };
-                                        put(_k, _v);
-                                }
+                    if (++done === MAX) {
+                        function d_cb(derr) {
+                            assert.ifError(derr);
+                            client.close();
                         }
+                        client.deleteBucket(BUCKET,
+                                            d_cb);
+                    }
+                }
 
-                        run();
-                });
+                function put(k, v) {
+                    function p_cb(err) {
+                        assert.ifError(err);
+                        client.getObject(BUCKET, k, cb);
+                    }
+                    client.putObject(BUCKET, k, v, p_cb);
+                }
+
+                for (var i = 0; i < MAX; i++) {
+                    var _k = libuuid.create();
+                    var _v = {
+                        foo: '' + i
+                    };
+                    put(_k, _v);
+                }
+            }
+
+            run();
         });
+    });
 })();
--- test/stub.test.js
+++ test/stub.test.js
@@ -1,6 +1,6 @@
 // Copyright 2012 Joyent, Inc.  All rights reserved.
 
-if (require.cache[__dirname + '/helper.js'])
+    if (require.cache[__dirname + '/helper.js'])
         delete require.cache[__dirname + '/helper.js'];
 var helper = require('./helper.js');
 
@@ -15,5 +15,5 @@ var test = helper.test;
 ///--- Tests
 
 test('stub', function (t) {
-        t.end();
+    t.end();
 });
